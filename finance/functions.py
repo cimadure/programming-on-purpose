6f9146e6d911a8577400434318967f18d939e207
cimadure <ronan.cimadure@gmail.com>
2018-08-09 19:29:54 -0600

    rescale by part with assert

--- finance/functions.py
+++ finance/functions.py
@@ -4,3 +4,15 @@ import numpy as np
 def last_maximum_occurrence_index(x):
     b = x[::-1]
     return len(b) - np.argmax(b) - 1
+
+
+def rescale_by_part(x, first, minimum, maximum):
+    if x >= first:
+        return (x-first)/(maximum-first)
+    else:
+        assert x >= minimum , "input is bellow minimum contraint"
+        return (x - first) / (first - minimum)
+
+
+def rescale(x):
+    return rescale_by_part(x=x, first=x[0], minimum=min(x), maximum=max(x))
--- finance/test/test_functions.py
+++ finance/test/test_functions.py
@@ -2,16 +2,48 @@ import numpy.testing as nt
 import numpy as np
 import unittest
 
-from finance.functions import last_maximum_occurrence_index
+from finance.functions import last_maximum_occurrence_index, rescale, rescale_by_part
 
 
-class TestFunctions(unittest.TestCase):
-    def test_something(self):
-        nt.assert_array_equal([1.0, np.pi, np.nan], [1, np.pi, np.nan])
+class TestMaximal(unittest.TestCase):
 
     def test_last_maximum_occurrence_with_1D_array(self):
         a = np.array([0, 0, 4, 4, 4, 4, 2, 2, 2, 2])
         self.assertEqual(5, last_maximum_occurrence_index(a))
+
+
+class TestRescaling(unittest.TestCase):
+
+    def test_rescale_by_part_maximum(self):
+        r = rescale_by_part(x=2., first=1., minimum=0.7, maximum=2.)
+        self.assertEqual(1, r)
+
+    def test_rescale_by_part_above(self):
+        r = rescale_by_part(x=2, first=1.0, minimum=0.7, maximum=4.0)
+        self.assertAlmostEqual(1./3, r)
+
+    def test_rescale_by_part_idem(self):
+        r = rescale_by_part(x=1.0, first=1.0, minimum=0.7, maximum=4.0)
+        self.assertAlmostEqual(0., r)
+
+    def test_rescale_by_part_minimum(self):
+        r = rescale_by_part(x=0.7, first=1.0, minimum=0.7, maximum=4.0)
+        self.assertAlmostEqual(-1., r)
+
+    def test_rescale_by_part_bellow_assertion(self):
+        with self.assertRaises(AssertionError) as context:
+            rescale_by_part(x=0.5, first=1.0, minimum=0.7, maximum=4.0)
+
+    def test_rescale_by_part_bellow(self):
+        r = rescale_by_part(x=0.7, first=1.0, minimum=0.5, maximum=4.0)
+        self.assertAlmostEqual(-0.6, r)
+
+
+    def test_rescale(self):
+        a = np.array([2, 1, 3, 4])
+        result = np.array([0, -1, 0.5, 1])
+
+        nt.assert_almost_equal(result, rescale(a))
 
 
 if __name__ == '__main__':

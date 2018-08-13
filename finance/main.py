ee8d6612c8a2ca960d35754f43c3d760145ef5e9
cimadure <ronan.cimadure@gmail.com>
2018-08-12 23:50:32 -0600

    rescaled graphic with x axis

--- finance/main.py
+++ finance/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from finance.functions import rescale
+from finance.functions import rescale, last_maximum_occurrence_index, rescale_timestamp_by_position
 
 np.random.seed(123456)
 
@@ -25,19 +25,32 @@ np.random.seed(123456)
 
 #------------------------------------------------------
 
-ticker = ['data/historical/Stocks/aapl.us.txt', 'data/historical/Stocks/yelp.us.txt']
-df = pd.read_csv(ticker[1], engine='python')
-print(df.head(4))
-print(df.tail(4))
+tickers = ['data/historical/Stocks/aapl.us.txt', 'data/historical/Stocks/yelp.us.txt']
+for ticker in tickers:
+    df = pd.read_csv(ticker, engine='python',  parse_dates=['Date'])
+    #print(df.head(4))
+    #print(df.tail(4))
+
+    #index = last_maximum_occurrence_index(df['Close'])
+    index = df['Close'].idxmax()
+
+    df['stamp'] = rescale_timestamp_by_position(df['Date'], index)
+
+    plt.figure()
+    df.plot(y='Close', title=ticker)
+    #plt.plot(index, df['Close'][index], 'rx')
+    plt.show()
+
+    df['Close'] = rescale(df['Close'])
+
+    plt.figure()
+    df.plot(x='stamp', y='Close')
+    plt.plot(df['stamp'][index], df['Close'][index], 'rx')
+    plt.show()
 
 
-plt.figure()
-df.plot(y='Close')
-plt.show()
 
-df['Close'] = rescale(df['Close'])
 
-plt.figure()
-df.plot(y='Close')
-plt.show()
+    #index = df['Close'].idxmax()
 
+    print("---+", index, df['Close'][index], df['Date'][index])
